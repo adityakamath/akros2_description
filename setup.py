76b227d4f20eae0133f53a519274c38a084b5de4
adityakamath <adityakamath@live.com>
2023-04-01 18:25:53 +0000

    updated collisions, moved to xacro, updated config for rviz and unity

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf.xacro')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
         (os.path.join('share', package_name, 'meshes'), glob('meshes/*.dae')),
     ],
     install_requires=['setuptools'],
